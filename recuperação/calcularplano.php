f4c4eb75b91ad848e2ba454d977a115bb3d9807a
herikingkkj <herrickfabbricio@gmail.com>
2025-12-15 13:25:45 -0300

    Add files via upload

---
+++ recuperação/calcularplano.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cálculo do Plano</title>
+</head>
+<body>
+    <header>Pet Care</header>
+    <h1>Cálculo do Plano</h1>
+    <p>Para calcular o plano ideal para o seu pet, preencha as informações abaixo:</p>
+
+   
